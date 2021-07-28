8f527a0b1f6b0ca0ec75df92a6c410b139fd190e
deependuprasad <68602972+deependuprasad@users.noreply.github.com>
2021-07-28 22:55:26 +0530

    added script.sh to simplify steps

---
+++ script.sh
@@ -0,0 +1,8 @@
+pip install virtualenv
+mkdir flaskappenv
+python3 -m venv flaskappenv
+source flaskappenv/bin/activate
+pip install -r requirements.txt
+export FLASK_APP=app.py
+export FLASK_DEBUG=1
+flask run
